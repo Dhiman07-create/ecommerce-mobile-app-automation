8ff3acaf5ce616c7aaf11361b399c55716eb29c3
Dhiman Dasgupta <70462119+Dhiman07-create@users.noreply.github.com>
2025-12-31 19:53:23 +0530

    Self hosted runner update

--- src/test/java/tests/SmokeTest.java
+++ src/test/java/tests/SmokeTest.java
@@ -7,12 +7,6 @@ import pages.HomePage;
 public class SmokeTest extends BaseTest {
 
     @Test
-    public void verifyAppLaunch() {
-        HomePage home = new HomePage(driver);
-        home.verifyHomePageLoaded();
-    }
-
-    @Test
     public void launchTest() {
         System.out.println(driver.getPageSource());
     }
